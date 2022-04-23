23df8b0205940f2ebee40459385e7015590eca77
Nurmukhamed Artykaly <nurmukhamed.artykaly@hdfilm.kz>
2022-04-23 15:21:04 +0600

    drupal config

---
+++ chapter6/drupal/outputs.tf
@@ -0,0 +1,17 @@
+resource "local_file" "ansible_inventory" {
+  content = templatefile("templates/inventory.tmpl",
+    {
+      drupal_ip = libvirt_domain.drupal.network_interface.0.addresses.0
+    }
+  )
+  filename = "inventory"
+}
+
+resource "local_file" "bash_config" {
+  content = templatefile("templates/config.sh.tmpl",
+    {
+      pvt_key = var.pvt_key
+    }
+  )
+  filename = "config.sh"
+}
---
+++ chapter6/solr.tf
@@ -0,0 +1,58 @@
+resource "libvirt_volume" "disk_solr" {
+  name           = "disk-solr"
+  base_volume_id = libvirt_volume.os_image_ubuntu.id
+  pool           = libvirt_pool.ansible.name
+  size           = 5361393152
+}
+
+data "template_file" "user_data_solr" {
+  template = file("${path.module}/templates/cloud_init_ubuntu.cfg.tmpl")
+  vars = {
+    server_hostname = "solr.test"
+  }
+}
+
+resource "libvirt_cloudinit_disk" "commoninit-solr" {
+  name           = "commoninit-solr.iso"
+  user_data      = data.template_file.user_data_solr.rendered
+  network_config = data.template_file.network_config_ubuntu.rendered
+  pool           = libvirt_pool.ansible.name
+}
+
+# Create the machine
+resource "libvirt_domain" "solr" {
+  name   = "solr"
+  memory = "2048"
+  vcpu   = 1
+
+  cloudinit = libvirt_cloudinit_disk.commoninit-solr.id
+
+  network_interface {
+    network_name = "default"
+    wait_for_lease = true
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.disk_solr.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
+# IPs: use wait_for_lease true or after creation use terraform refresh and terraform show for the ips of domain
