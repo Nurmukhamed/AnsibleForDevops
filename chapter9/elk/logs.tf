4ad38f3f14d133ca2b74a1632d7d76c0c174fe95
Nurmukhamed Artykaly <nurmukhamed.artykaly@hdfilm.kz>
2022-05-06 18:24:52 +0600

    chapter9 - elk is ready

---
+++ chapter9/elk/logs.tf
@@ -0,0 +1,58 @@
+resource "libvirt_volume" "disk_logs" {
+  name           = "disk-logs"
+  base_volume_id = libvirt_volume.os_image_ubuntu.id
+  pool           = libvirt_pool.ansible.name
+  size           = 15361393152
+}
+
+data "template_file" "user_data_logs" {
+  template = file("${path.module}/../../templates/cloud_init_ubuntu.cfg.tmpl")
+  vars = {
+    server_hostname = "logs.test"
+  }
+}
+
+resource "libvirt_cloudinit_disk" "commoninit-logs" {
+  name           = "commoninit-logs.iso"
+  user_data      = data.template_file.user_data_logs.rendered
+  network_config = data.template_file.network_config_ubuntu.rendered
+  pool           = libvirt_pool.ansible.name
+}
+
+# Create the machine
+resource "libvirt_domain" "logs" {
+  name   = "logs"
+  memory = "4096"
+  vcpu   = 2
+
+  cloudinit = libvirt_cloudinit_disk.commoninit-logs.id
+
+  network_interface {
+    network_name = "default"
+    wait_for_lease = true
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.disk_logs.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
+# IPs: use wait_for_lease true or after creation use terraform refresh and terraform show for the ips of domain
---
+++ chapter9/elk/outputs.tf
@@ -0,0 +1,18 @@
+resource "local_file" "ansible_inventory" {
+  content = templatefile("templates/inventory.tmpl",
+    {
+      logs_ip = libvirt_domain.logs.network_interface.0.addresses.0
+      web_ip = libvirt_domain.web.network_interface.0.addresses.0
+    }
+  )
+  filename = "inventory"
+}
+
+resource "local_file" "bash_config" {
+  content = templatefile("${path.module}/../../templates/config.sh.tmpl",
+    {
+      pvt_key = var.pvt_key
+    }
+  )
+  filename = "config.sh"
+}
